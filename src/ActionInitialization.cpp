9c6c9cd4f2be5402fd1b7520848abcb4921e74a2
Sarah Creasman <sjj158@vols.utk.edu>
2021-10-26 16:15:54 -0400

    Added ActionInitialization

---
+++ src/ActionInitialization.cpp
@@ -0,0 +1,11 @@
+#include "ActionInitialization.hpp"
+
+namespace ne697 {
+  void ActionInitialization::BuildForMaster() const {
+    return;
+  }
+  
+  void ActionInitialization::Build() const {
+    return;
+  }
+}
